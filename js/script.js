0bc65e2fdc98824371e687a11af97de2adb88b86
r-domashchenko <therostofan@gmail.com>
2022-08-03 20:20:58 +0300

    2 steps added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 use strict;
+
+const numberOfFilms = +prompt(`Скільки фільмів ви вже подивились?`, ``);
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
