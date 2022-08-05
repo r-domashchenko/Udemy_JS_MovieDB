3252c40c581043720727e0d79080cd0e4570fff2
r-domashchenko <therostofan@gmail.com>
2022-08-05 16:58:11 +0300

    Lesson 2.4 done - conditions for questions added

--- js/script.js
+++ js/script.js
@@ -11,9 +11,24 @@ const personalMovieDb = {
 }
 
 for (let i = 0; i < numberOfFilms; i++) {
-    lastFilm = prompt(`Останній фільм`, ``);
-    numberOfStars = prompt(`На скільки зірок оціните?`, ``);
-    personalMovieDb.movies[lastFilm] = numberOfStars;
+    const lastFilm = prompt(`Останній фільм`, ``),
+        numberOfStars = prompt(`На скільки зірок оціните?`, ``);
+
+    if (lastFilm != null && numberOfStars != null && lastFilm != '' && numberOfStars != '' && lastFilm.length < 50) {
+        personalMovieDb.movies[lastFilm] = numberOfStars;
+    } else {
+        i--;
+    }
 }
 
-console.log(personalMovieDb.movies);
+if (personalMovieDb.count <= 10) {
+    console.log('дуже мало дивились');
+} else if (personalMovieDb.count > 10 && personalMovieDb.count <= 30) {
+    console.log('знаєтесь у фільмах');
+} else if (personalMovieDb.count > 31) {
+    console.log('Ви - кіноман!');
+} else {
+    console.log('error');
+}
+
+console.log(personalMovieDb);
