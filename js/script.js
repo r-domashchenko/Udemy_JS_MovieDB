4aa7588e79b78e3ea09a162dbd917db8661ff62f
r-domashchenko <therostofan@gmail.com>
2022-08-09 18:15:59 +0300

    Array hint added

--- js/script.js
+++ js/script.js
@@ -1,34 +1,72 @@
 "use strict";
 
-const numberOfFilms = +prompt(`Скільки фільмів ви вже подивились?`, ``);
+function getMathResult(num, progression) {
 
-const personalMovieDb = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}
-
-for (let i = 0; i < numberOfFilms; i++) {
-    const lastFilm = prompt(`Останній фільм`, ``),
-        numberOfStars = prompt(`На скільки зірок оціните?`, ``);
-
-    if (lastFilm != null && numberOfStars != null && lastFilm != '' && numberOfStars != '' && lastFilm.length < 50) {
-        personalMovieDb.movies[lastFilm] = numberOfStars;
-    } else {
-        i--;
+    if (typeof (progression) !== 'number' || progression <= 0) {
+        console.log(num);
+        return num;
+        
     }
+
+    let str = '';
+
+    for (let i = 1; i <= progression; i++) {
+        if (i === progression) {
+            str += `${num * i}`;
+        } else {
+            str += `${num * i}---`;
+        }
+    }
+    console.log(str);
+    return str;
 }
 
-if (personalMovieDb.count <= 10) {
-    console.log('дуже мало дивились');
-} else if (personalMovieDb.count > 10 && personalMovieDb.count <= 30) {
-    console.log('знаєтесь у фільмах');
-} else if (personalMovieDb.count > 31) {
-    console.log('Ви - кіноман!');
-} else {
-    console.log('error');
+getMathResult(10, 5);
+
+
+// Exercise 7*
+
+function calculateVolumeAndArea(length) {
+    if (typeof (length) !== 'number' || length <= 0 || !Number.isInteger(length)) {
+        return "При вычислении произошла ошибка";
+    }
+        
+    let volume = 0,
+        square = 0;
+
+    volume = length * length * length;
+    square = 6 * (length * length);
+    return `Объем куба: ${volume}, площадь всей поверхности: ${square}`;
 }
 
-console.log(personalMovieDb);
+console.log(calculateVolumeAndArea.volume);
+calculateVolumeAndArea(6);
+
+
+function getCoupeNumber(num) {
+    if (typeof (num) !== 'number' || !Number.isInteger(num) || num < 0) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+
+    if (num === 0 || num > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+    return Math.ceil(num / 4);
+
+}
+
+// Exercise 8*
+
+function getTimeFromMinutes(time) {
+
+    if (typeof (time) !== 'number' || !Number.isInteger(time) || time < 0) {
+        return "Ошибка, проверьте данные";
+    }
+
+}
+
+getTimeFromMinutes(150);
+
+
+
+
