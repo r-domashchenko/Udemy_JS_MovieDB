5a6eb4690f8db25a5f90076cb41bde1f586bae3c
r-domashchenko <therostofan@gmail.com>
2022-08-10 19:00:25 +0300

    Refactoring - all function to method of personalMovieDB

--- js/script.js
+++ js/script.js
@@ -2,67 +2,68 @@
 
 let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt(`Скільки фільмів ви вже подивились?`, ``);
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt(`Скільки фільмів ви вже подивились?`, ``);
-    }
-}
-
-start();
-
 const personalMovieDb = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-}
+    privat: false,
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDb);
-    }
-}
+    start: function () {
+        numberOfFilms = +prompt(`Скільки фільмів ви вже подивились?`, ``);
+        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt(`Скільки фільмів ви вже подивились?`, ``);
+        }
+    },
 
-showMyDB(personalMovieDb.privat);
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDb);
+        }
+    },
 
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDb.genres[i - 1] = prompt(`Ваш улюблений жанр під номером: &{i}`);
-    }
-}
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            personalMovieDb.genres[i - 1] = prompt(`Ваш улюблений жанр під номером: &{i}`);
+        }
+    },
 
-writeYourGenres
+    rememberMyFilms: function() {
+        for (let i = 0; i < numberOfFilms; i++) {
+            const lastFilm = prompt(`Останній фільм`, ``).trim(),
+                numberOfStars = prompt(`На скільки зірок оціните?`, ``);
 
-function rememberMyFilms() {
-    for (let i = 0; i < numberOfFilms; i++) {
-        const lastFilm = prompt(`Останній фільм`, ``),
-            numberOfStars = prompt(`На скільки зірок оціните?`, ``);
+            if (lastFilm != null && numberOfStars != null && lastFilm != '' && numberOfStars != '' && lastFilm.length < 50) {
+                personalMovieDb.movies[lastFilm] = numberOfStars;
+            } else {
+                i--;
+            }
+        }
+    },
 
-        if (lastFilm != null && numberOfStars != null && lastFilm != '' && numberOfStars != '' && lastFilm.length < 50) {
-            personalMovieDb.movies[lastFilm] = numberOfStars;
+    detectPersonalLevel: function() {
+        if (personalMovieDb.count <= 10) {
+            console.log('дуже мало дивились');
+        } else if (personalMovieDb.count > 10 && personalMovieDb.count <= 30) {
+            console.log('знаєтесь у фільмах');
+        } else if (personalMovieDb.count > 31) {
+            console.log('Ви - кіноман!');
         } else {
-            i--;
+            console.log('error');
+        }
+    },
+
+    detectPersonalLevel: function() {
+        if (personalMovieDb.count <= 10) {
+            console.log('дуже мало дивились');
+        } else if (personalMovieDb.count > 10 && personalMovieDb.count <= 30) {
+            console.log('знаєтесь у фільмах');
+        } else if (personalMovieDb.count > 31) {
+            console.log('Ви - кіноман!');
+        } else {
+            console.log('error');
         }
     }
 }
 
-rememberMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMovieDb.count <= 10) {
-        console.log('дуже мало дивились');
-    } else if (personalMovieDb.count > 10 && personalMovieDb.count <= 30) {
-        console.log('знаєтесь у фільмах');
-    } else if (personalMovieDb.count > 31) {
-        console.log('Ви - кіноман!');
-    } else {
-        console.log('error');
-    }
-}
-
-detectPersonalLevel();
-
-console.log(personalMovieDb);
